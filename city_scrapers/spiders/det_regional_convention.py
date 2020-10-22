c918e337c6b39b8d1bd3ef6cc6b8e89a2336dac9
Shayan Fahimi <shayan2008@gmail.com>
2020-10-22 10:46:48 -0700

    add 0003-det-regional-convention (#6)
    * add 0003-det-regional-convention
    
    * archived webpage with 2020 meetings are parsed and added
    
    * Board of Directors for all old meetings, new meetings title is parsed
    
    * tiny problem in testing
    
    * future_meeting links return empty list + print removed
    
    Co-authored-by: Shayan <shayan@Home-iMac.localdomain>

---
+++ city_scrapers/spiders/det_regional_convention.py
@@ -0,0 +1,153 @@
+import re
+from datetime import datetime, time
+
+from city_scrapers_core.constants import BOARD
+from city_scrapers_core.items import Meeting
+from city_scrapers_core.spiders import CityScrapersSpider
+
+
+class DetRegionalConventionSpider(CityScrapersSpider):
+    name = "det_regional_convention"
+    agency = "Detroit Regional Convention Facility Authority"
+    timezone = "America/Detroit"
+    start_urls = ["http://www.drcfa.org/upcoming-meetings"]
+
+    def parse(self, response):
+        """
+        `parse` should always `yield` Meeting items.
+        """
+        # Scraping upcoming meetings if they become available
+        future_meeting = True
+        for item in response.xpath(
+            "//div[@class='textarea']//div[@class='content clearfix']"
+        )[1:]:
+            title = self._parse_title(item, future_meeting)
+            for meeting in item.xpath(
+                "../following-sibling::div[1][@class='events']//\
+                div[@class='event_list']//div[@class='info clearfix']//h3"
+            ):
+                start = self._parse_start(meeting, future_meeting)
+                if None in (start, title):
+                    continue
+                meeting = Meeting(
+                    title=title,
+                    description=self._parse_description(item),
+                    classification=self._parse_classification(item),
+                    start=start,
+                    end=self._parse_end(item),
+                    all_day=self._parse_all_day(item),
+                    time_notes=self._parse_time_notes(item),
+                    location=self._parse_location(item),
+                    links=self._parse_links(item, future_meeting),
+                    source=self._parse_source(response),
+                )
+
+                meeting["status"] = self._get_status(meeting)
+                meeting["id"] = self._get_id(meeting)
+
+                yield meeting
+
+        future_meeting = False
+        for item in response.xpath("//div[@class='link']/ul/li/a"):
+            start = self._parse_start(item, future_meeting)
+            if start is None:
+                continue
+            meeting = Meeting(
+                title=self._parse_title(item, future_meeting),
+                description=self._parse_description(item),
+                classification=self._parse_classification(item),
+                start=start,
+                end=self._parse_end(item),
+                all_day=self._parse_all_day(item),
+                time_notes=self._parse_time_notes(item),
+                location=self._parse_location(item),
+                links=self._parse_links(item, future_meeting),
+                source=self._parse_source(response),
+            )
+
+            meeting["status"] = self._get_status(meeting)
+            meeting["id"] = self._get_id(meeting)
+
+            yield meeting
+
+    def _parse_title(self, item, future_meeting):
+        """Parse or generate meeting title."""
+        if future_meeting:
+            for announcement in item.xpath(".//h1/text()"):
+                meeting_announce = announcement.get().split()
+                remove_words = ["dates", "schedule"]
+                title_list = [
+                    word
+                    for word in meeting_announce
+                    if word.lower() not in remove_words
+                ]
+                return " ".join(title_list)
+        else:
+            return "Board of Directors"
+
+    def _parse_description(self, item):
+        """Parse or generate meeting description."""
+        return ""
+
+    def _parse_classification(self, item):
+        """Parse or generate classification from allowed options."""
+        return BOARD
+
+    def _parse_start(self, item, future_meeting):
+        """Parse start datetime as a naive datetime object."""
+        if future_meeting:
+            time_unparsed = item.xpath("text()").getall()[2]
+            time_struct = r"(\d{1,2}):(\d{2})(\s*[ap]m?)"
+            time_str = re.search(time_struct, time_unparsed)
+            date_str = " ".join(item.xpath(".//span/text()").getall())
+            if time_str and date_str:
+                datetime_str = date_str + " " + time_str.group(0)
+                datetime_object = datetime.strptime(datetime_str, "%b %d , %Y %I:%M%p")
+                return datetime_object
+            else:
+                return None
+        else:
+            default_starttime = time(8, 30)
+            report_name = item.xpath("./@href")[0].get().split("/")[-1]
+            name_splitted = report_name.replace("-", ".").replace("_", ".").split(".")
+            date_digits = [int(digit) for digit in name_splitted if digit.isdigit()]
+            if len(date_digits) == 3:
+                date_obj = datetime(
+                    2000 + int(str(date_digits[2])[:2]), date_digits[0], date_digits[1]
+                )
+            else:
+                # One meeting does not have similar naming structure
+                return None
+            return datetime.combine(date_obj, default_starttime)
+
+    def _parse_end(self, item):
+        """Parse end datetime as a naive datetime object. Added by pipeline if None"""
+        return None
+
+    def _parse_time_notes(self, item):
+        """Parse any additional notes on the timing of the meeting"""
+        return ""
+
+    def _parse_all_day(self, item):
+        """Parse or generate all-day status. Defaults to False."""
+        return False
+
+    def _parse_location(self, item):
+        """Parse or generate location."""
+        return {
+            "address": "1 Washington Blvd, Detroit, MI 48226",
+            "name": "TCF Center",
+        }
+
+    def _parse_links(self, item, future_meeting):
+        """Parse or generate links."""
+        if future_meeting:
+            return []
+        else:
+            report_link = item.xpath("./@href")[0].get()
+            report_desc = item.xpath("./@title")[0].get()
+            return [{"href": report_link, "title": report_desc}]
+
+    def _parse_source(self, response):
+        """Parse or generate source."""
+        return response.url
---
+++ tests/test_det_regional_convention.py
@@ -0,0 +1,86 @@
+from datetime import datetime
+from os.path import dirname, join
+
+import pytest
+from city_scrapers_core.constants import BOARD, PASSED
+from city_scrapers_core.utils import file_response
+from freezegun import freeze_time
+
+from city_scrapers.spiders.det_regional_convention import DetRegionalConventionSpider
+
+test_response = file_response(
+    join(dirname(__file__), "files", "det_regional_convention.html"),
+    url="http://www.drcfa.org/upcoming-meetings",
+)
+spider = DetRegionalConventionSpider()
+
+freezer = freeze_time("2020-10-20")
+freezer.start()
+
+parsed_items = [item for item in spider.parse(test_response)]
+
+freezer.stop()
+# Since the archived webpage is used for testing
+test_number = 0
+
+
+def test_title():
+    assert parsed_items[test_number]["title"] == "Board of Directors"
+
+
+def test_description():
+    assert parsed_items[test_number]["description"] == ""
+
+
+def test_start():
+    assert parsed_items[test_number]["start"] == datetime(2015, 1, 29, 8, 30)
+
+
+def test_end():
+    assert parsed_items[test_number]["end"] is None
+
+
+def test_time_notes():
+    assert parsed_items[test_number]["time_notes"] == ""
+
+
+def test_id():
+    assert (
+        parsed_items[test_number]["id"]
+        == "det_regional_convention/201501290830/x/" + "board_of_directors"
+    )
+
+
+def test_status():
+    assert parsed_items[test_number]["status"] == PASSED
+
+
+def test_location():
+    assert parsed_items[test_number]["location"] == {
+        "address": "1 Washington Blvd, Detroit, MI 48226",
+        "name": "TCF Center",
+    }
+
+
+def test_source():
+    assert (
+        parsed_items[test_number]["source"] == "http://www.drcfa.org/upcoming-meetings"
+    )
+
+
+def test_links():
+    assert parsed_items[test_number]["links"] == [
+        {
+            "href": "http://www.drcfa.org/assets/doc/Approved-Minutes-1-29-15.pdf",
+            "title": "Approved DRCFA Minutes January 29, 2015",
+        }
+    ]
+
+
+def test_classification():
+    assert parsed_items[test_number]["classification"] == BOARD
+
+
+@pytest.mark.parametrize("item", parsed_items)
+def test_all_day(item):
+    assert item["all_day"] is False
